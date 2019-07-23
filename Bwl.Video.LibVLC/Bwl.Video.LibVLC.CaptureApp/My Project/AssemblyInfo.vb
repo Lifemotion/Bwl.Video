b367394dbbfb670b716d73c4a2bdba9280fd056f
draf <artem.drobanov@gmail.com>
2019-07-23 17:02:17 +0300

    Корректировка игнора AssemblyInfo после маркировки.

---
+++ Bwl.Video.LibVLC/Bwl.Video.LibVLC.CaptureApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,36 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором 
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("Bwl.Video.LibVLC.CaptureApp")>
+<Assembly: AssemblyDescription("1b81e58 Маркировка версии.")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Bwl.Video.LibVLC.CaptureApp")>
+<Assembly: AssemblyCopyright("Copyright ©  2015")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("78b55583-46bb-42bc-ba17-f905364d38e3")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.2019.0520.1545")>
+<Assembly: AssemblyFileVersion("1.2019.0520.1545")>
+<Assembly: AssemblyInformationalVersion("{c:ymd.} {c:hms:} {chash:6}")>
---
+++ Bwl.Video.LibVLC/Bwl.Video.LibVLC/Bwl.Video/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором 
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("Bwl.Video")> 
+<Assembly: AssemblyDescription("1b81e58 Маркировка версии.")>
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Bwl.Video")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("96e469e3-3040-428a-ad51-28e897ca32d1")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.2019.0520.1545")>
+<Assembly: AssemblyFileVersion("1.2019.0520.1545")>
